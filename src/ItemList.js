f2e06582f9c356970fbd289b307391ccb2f6a4a5
egesenli <egesenli@gmail.com>
2023-04-11 16:09:24 -0400

    Update ItemList.js props to use destructuring and fix LineItem rendering

--- src/ItemList.js
+++ src/ItemList.js
@@ -1,17 +1,19 @@
 import React from "react";
 import LineItem from "./LineItem";
 
-const ItemList = ({ item, handleCheck, handleDelete }) => {
+const ItemList = ({ items, handleCheck, handleDelete }) => {
   return (
     <ul>
-        <LineItem 
-            key={item.id}
-            item={item}
-            handleCheck={handleCheck}
-            handleDelete={handleDelete}
-            />
+      {items.map((item) => (
+        <LineItem
+          key={item.id}
+          item={item}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
+      ))}
     </ul>
-  )
-}
+  );
+};
 
 export default ItemList;
